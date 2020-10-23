a38ce903f286834e173b421f176ab88adc3c1608
Petri Silen <petri.silen@silensoft.com>
2020-10-23 11:59:04 +0300

    Refactoring

---
+++ src/backk/validation/setNestedTypeValidationDecorators.ts
@@ -0,0 +1,41 @@
+import { getFromContainer, MetadataStorage, ValidationTypes } from 'class-validator';
+import { ValidationMetadata } from 'class-validator/metadata/ValidationMetadata';
+import { ValidationMetadataArgs } from 'class-validator/metadata/ValidationMetadataArgs';
+import { Type } from 'class-transformer';
+
+export default function setNestedTypeValidationDecorators(
+  typeClass: Function,
+  targetAndPropNameToHasNestedValidationMap: { [key: string]: boolean }
+) {
+  const validationMetadatas = getFromContainer(MetadataStorage).getTargetValidationMetadatas(typeClass, '');
+
+  validationMetadatas.forEach((validationMetadata: ValidationMetadata) => {
+    if (validationMetadata.type === 'isInstance') {
+      const nestedValidationMetadataArgs: ValidationMetadataArgs = {
+        type: ValidationTypes.NESTED_VALIDATION,
+        target: validationMetadata.target,
+        propertyName: validationMetadata.propertyName,
+        validationOptions: { each: validationMetadata.each }
+      };
+
+      if (
+        !targetAndPropNameToHasNestedValidationMap[
+          (validationMetadata.target as Function).name + validationMetadata.propertyName
+        ]
+      ) {
+        Type(() => validationMetadata.constraints[0])(
+          new (validationMetadata.target as new () => any)(),
+          validationMetadata.propertyName
+        );
+
+        getFromContainer(MetadataStorage).addValidationMetadata(
+          new ValidationMetadata(nestedValidationMetadataArgs)
+        );
+
+        targetAndPropNameToHasNestedValidationMap[
+          (validationMetadata.target as Function).name + validationMetadata.propertyName
+        ] = true;
+      }
+    }
+  });
+}
