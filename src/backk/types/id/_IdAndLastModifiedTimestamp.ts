965bbcaae5d989c78ce7a931122b0f74037e5516
Petri Silen <petri.silen@silensoft.com>
2020-11-21 13:25:24 +0200

    Added _IdAndLastModifiedTimestamp

---
+++ src/backk/types/id/_IdAndLastModifiedTimestamp.ts
@@ -0,0 +1,6 @@
+import _Id from "./_Id";
+
+// eslint-disable-next-line @typescript-eslint/class-name-casing
+export default class _IdAndLastModifiedTimestamp extends _Id {
+  lastModifiedTimestamp!: Date;
+}
