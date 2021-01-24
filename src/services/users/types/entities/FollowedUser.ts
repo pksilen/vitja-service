74c9a50e1f078e91653f0a657e5de75ba9c5e2e4
Petri Silen <petri.silen@silensoft.com>
2021-01-24 16:46:41 +0200

    Added files

---
+++ src/services/users/types/entities/FollowedUser.ts
@@ -0,0 +1,29 @@
+// This is an auto-generated file from the respective .type file
+// DO NOT MODIFY THIS FILE! Updates should be made to the respective .type file only
+// This file can be generated from the respective .type file by running npm script 'generateTypes'
+
+import { MaxLength } from 'class-validator';
+import Entity from '../../../../backk/decorators/entity/Entity';
+import IsStringOrObjectId from '../../../../backk/decorators/typeproperty/IsStringOrObjectId'; // eslint-disable-next-line @typescript-eslint/class-name-casing
+import IsUndefined from '../../../../backk/decorators/typeproperty/IsUndefined';
+import MaxLengthAndMatches from '../../../../backk/decorators/typeproperty/MaxLengthAndMatches';
+import { SalesItem } from '../../../salesitems/types/entities/SalesItem';
+
+@Entity('User')
+export default class FollowedUser {
+  @IsUndefined({
+    groups: ['__backk_create__']
+  })
+  @IsStringOrObjectId({
+    groups: ['__backk_update__']
+  })
+  @MaxLengthAndMatches(24, /^[a-f\d]+$/, {
+    groups: ['__backk_update__']
+  })
+  _id!: string;
+
+  @MaxLength(512)
+  public displayName!: string;
+
+  public readonly salesItems!: SalesItem[];
+}
