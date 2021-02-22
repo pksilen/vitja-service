6b7baa0f2ec8e0cc077e02ffe9737e88c191025e
Petri Silen <petri.silen@silensoft.com>
2021-02-22 15:48:34 +0200

    Changes

--- src/services/order/types/args/PlaceOrderArg.ts
+++ src/services/order/types/args/PlaceOrderArg.ts
@@ -2,16 +2,10 @@
 // DO NOT MODIFY THIS FILE! Updates should be made to the respective .type file only
 // This file can be generated from the respective .type file by running npm script 'generateTypes'
 
-import MinMax from "../../../../backk/decorators/typeproperty/MinMax";
-import Entity from "../../../../backk/decorators/entity/Entity";
-import { IsAlphanumeric, IsNumber, MaxLength } from "class-validator"
-import { IsExternalId } from "../../../../backk/decorators/typeproperty/IsExternalId"
-import { Lengths, Values } from "../../../../backk/constants/constants"
-import { PaymentGateway } from "../enum/PaymentGateway"
-
-import ShoppingCart from "../../../shoppingcart/types/entities/ShoppingCart";
-import { IsUrl, MaxLength } from "class-validator";
-import { Lengths } from "../../../../backk/constants/constants";
+import { IsUrl, MaxLength } from 'class-validator';
+import { Lengths } from '../../../../backk/constants/constants';
+import ShoppingCart from '../../../shoppingcart/types/entities/ShoppingCart';
+import { PaymentGateway } from '../enum/PaymentGateway';
 
 export default class PlaceOrderArg {
   public paymentGateway: PaymentGateway = 'Paytrail';
@@ -21,5 +15,4 @@ export default class PlaceOrderArg {
   @MaxLength(Lengths._4K)
   @IsUrl()
   uiRedirectUrl!: string;
-
 }
--- src/services/useraccount/types/args/ChangeUserPasswordArg.ts
+++ src/services/useraccount/types/args/ChangeUserPasswordArg.ts
@@ -2,32 +2,12 @@
 // DO NOT MODIFY THIS FILE! Updates should be made to the respective .type file only
 // This file can be generated from the respective .type file by running npm script 'generateTypes'
 
-import Entity from "../../../../backk/decorators/entity/Entity";
-import PaymentMethod from "../entities/PaymentMethod";
-import Order from "../../../order/types/entities/Order";
-import IsAnyString from "../../../../backk/decorators/typeproperty/IsAnyString";
-import IsPostalCode from "../../../../backk/decorators/typeproperty/IsPostalCode";
-import IsOneOf from "../../../../backk/decorators/typeproperty/IsOneOf";
-import getCities from "../../validation/getCities";
-import IsDataUri from "../../../../backk/decorators/typeproperty/IsDataUri";
-import FavoriteSalesItem from "../entities/FavoriteSalesItem";
-import OwnSalesItem from "../entities/OwnSalesItem";
-import FollowUser from "../entities/FollowUser";
-import BaseUserAccount from "../../../../backk/types/useraccount/BaseUserAccount";
-import _IdAndCaptcha from "../../../../backk/types/id/_IdAndCaptcha";
-import IsStrongPassword from "../../../../backk/decorators/typeproperty/IsStrongPassword";
-import _Id from "../../../../backk/types/id/_Id";
-import MaxLengthAndMatches from "../../../../backk/decorators/typeproperty/MaxLengthAndMatches";
-import IsUndefined from "../../../../backk/decorators/typeproperty/IsUndefined";
-import IsStringOrObjectId from "../../../../backk/decorators/typeproperty/IsStringOrObjectId"; // eslint-disable-next-line @typescript-eslint/class-name-casing
-import { ArrayMaxSize, ArrayMinSize, IsPhoneNumber, MaxLength, IsEmail, IsAscii, IsString } from "class-validator"
-import { ManyToMany } from "../../../../backk/decorators/typeproperty/ManyToMany"
-import { Lengths } from "../../../../backk/constants/constants"
-import { ShouldBeTrueFor } from "../../../../backk/decorators/typeproperty/ShouldBeTrueFor"
-import { Unique } from "../../../../backk/decorators/typeproperty/Unique"
-import { Transient } from "../../../../backk/decorators/typeproperty/Transient"
-import { BackkEntity } from "../../../../backk/types/entities/BackkEntity"
-
+import { IsEmail } from 'class-validator';
+import IsStringOrObjectId from '../../../../backk/decorators/typeproperty/IsStringOrObjectId'; // eslint-disable-next-line @typescript-eslint/class-name-casing
+import IsStrongPassword from '../../../../backk/decorators/typeproperty/IsStrongPassword';
+import IsUndefined from '../../../../backk/decorators/typeproperty/IsUndefined';
+import MaxLengthAndMatches from '../../../../backk/decorators/typeproperty/MaxLengthAndMatches';
+import { Unique } from '../../../../backk/decorators/typeproperty/Unique';
 
 export default class ChangeUserPasswordArg {
   @IsUndefined({
@@ -54,5 +34,4 @@ export default class ChangeUserPasswordArg {
 
   @IsStrongPassword()
   currentPassword!: string;
-
 }
