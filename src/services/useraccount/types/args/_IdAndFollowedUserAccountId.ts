5492a6a385e2aea6d16190ecfdc89f4d4e4f35d4
Petri Silen <petri.silen@silensoft.com>
2021-03-01 15:16:37 +0200

    Changes

--- src/services/useraccount/types/args/_IdAndFollowedUserAccountId.ts
+++ src/services/useraccount/types/args/_IdAndFollowedUserAccountId.ts
@@ -10,5 +10,5 @@ export default class _IdAndFollowedUserAccountId extends _IdAndVersionAndDefault
   )
   userAccountId!: string;
 
-  includeResponseFields = ['followedUsers']
+  includeResponseFields: string[] = ['followedUsers']
 }
