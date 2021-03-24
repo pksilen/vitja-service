c500dca378ee27c50cfa051a96366c50379632f3
Petri Silen <petri.silen@silensoft.com>
2021-03-24 19:09:46 +0200

    Changes

--- src/services/shoppingcart/ShoppingCartServiceImpl.ts
+++ src/services/shoppingcart/ShoppingCartServiceImpl.ts
@@ -151,6 +151,7 @@ export default class ShoppingCartServiceImpl extends ShoppingCartService {
 
   @AllowForSelf()
   @Delete()
+  @TestSetup(['shoppingCartService.addToShoppingCart'])
   @PostTests([
     {
       testName: 'shopping cart is empty',
