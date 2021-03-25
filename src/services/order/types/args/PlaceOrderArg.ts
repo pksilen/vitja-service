73836643b1719207362d840e40f85dd928850d04
Petri Silen <petri.silen@silensoft.com>
2021-03-25 16:24:27 +0200

    Changes

--- src/services/order/types/args/PlaceOrderArg.ts
+++ src/services/order/types/args/PlaceOrderArg.ts
@@ -2,11 +2,32 @@
 // DO NOT MODIFY THIS FILE! Updates should be made to the respective .type file only
 // This file can be generated from the respective .type file by running npm script 'generateTypes'
 
-import { IsUrl, MaxLength } from 'class-validator';
-import { Lengths } from '../../../../backk/constants/constants';
-import IsStringOrObjectId from '../../../../backk/decorators/typeproperty/IsStringOrObjectId';
-import MaxLengthAndMatches from '../../../../backk/decorators/typeproperty/MaxLengthAndMatches'; // eslint-disable-next-line @typescript-eslint/class-name-casing
-import { PaymentGateway } from '../enum/PaymentGateway';
+import Entity from "../../../../backk/decorators/entity/Entity";
+import OrderItem from "../entities/OrderItem";
+import _IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestampAndUserAccountId from "../../../../backk/types/id/_IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestampAndUserAccountId";
+import MinMax from "../../../../backk/decorators/typeproperty/MinMax";
+import _IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestamp from "../../../../backk/types/id/_IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestamp";
+import IsStringOrObjectId from "../../../../backk/decorators/typeproperty/IsStringOrObjectId";
+import MaxLengthAndMatches from "../../../../backk/decorators/typeproperty/MaxLengthAndMatches"; // eslint-disable-next-line @typescript-eslint/class-name-casing
+import _IdAndVersionAndCreatedAtTimestamp from "../../../../backk/types/id/_IdAndVersionAndCreatedAtTimestamp";
+import IsUndefined from "../../../../backk/decorators/typeproperty/IsUndefined";
+import _IdAndVersion from "../../../../backk/types/id/_IdAndVersion";
+import IsUndefined from "../../../../backk/decorators/typeproperty/IsUndefined"; // eslint-disable-next-line @typescript-eslint/class-name-casing
+import _Id from "../../../../backk/types/id/_Id";
+import IsIntegerStringOrAny from "../../../../backk/decorators/typeproperty/IsIntegerStringOrAny"; // eslint-disable-next-line @typescript-eslint/class-name-casing
+import MaxLengthAndMatches from "../../../../backk/decorators/typeproperty/MaxLengthAndMatches";
+import IsStringOrObjectId from "../../../../backk/decorators/typeproperty/IsStringOrObjectId"; // eslint-disable-next-line @typescript-eslint/class-name-casing
+import { ArrayMaxSize, ArrayMinSize, IsAlphanumeric, MaxLength, IsDate, IsString } from "class-validator"
+import { Lengths, Values } from "../../../../backk/constants/constants"
+import { OneToMany } from "../../../../backk/decorators/typeproperty/OneToMany"
+import { PaymentGateway } from "../enum/PaymentGateway"
+import { IsExternalId } from "../../../../backk/decorators/typeproperty/IsExternalId"
+import { IsFloat } from "../../../../backk/decorators/typeproperty/IsFloat"
+import { BackkEntity } from "../../../../backk/types/entities/BackkEntity"
+
+import ShoppingCart from "../../../shoppingcart/types/entities/ShoppingCart";
+import { IsUrl, MaxLength } from "class-validator";
+import { Lengths } from "../../../../backk/constants/constants";
 
 export default class PlaceOrderArg {
   @IsStringOrObjectId()
@@ -18,4 +39,5 @@ export default class PlaceOrderArg {
   @MaxLength(Lengths._4K)
   @IsUrl()
   uiRedirectUrl!: string;
+
 }
--- src/services/useraccount/types/args/ChangeUserPasswordArg.ts
+++ src/services/useraccount/types/args/ChangeUserPasswordArg.ts
@@ -2,11 +2,35 @@
 // DO NOT MODIFY THIS FILE! Updates should be made to the respective .type file only
 // This file can be generated from the respective .type file by running npm script 'generateTypes'
 
-import { IsEmail, IsString, MaxLength } from 'class-validator';
-import IsStringOrObjectId from '../../../../backk/decorators/typeproperty/IsStringOrObjectId'; // eslint-disable-next-line @typescript-eslint/class-name-casing
-import IsStrongPassword from '../../../../backk/decorators/typeproperty/IsStrongPassword';
-import MaxLengthAndMatches from '../../../../backk/decorators/typeproperty/MaxLengthAndMatches';
-import { Unique } from '../../../../backk/decorators/typeproperty/Unique';
+import Entity from "../../../../backk/decorators/entity/Entity";
+import PaymentMethod from "../entities/PaymentMethod";
+import Order from "../../../order/types/entities/Order";
+import IsAnyString from "../../../../backk/decorators/typeproperty/IsAnyString";
+import IsPostalCode from "../../../../backk/decorators/typeproperty/IsPostalCode";
+import IsOneOf from "../../../../backk/decorators/typeproperty/IsOneOf";
+import getCities from "../../validation/getCities";
+import IsDataUri from "../../../../backk/decorators/typeproperty/IsDataUri";
+import BaseUserAccount from "../../../../backk/types/useraccount/BaseUserAccount";
+import FavoriteSalesItem from "../../../salesitem/types/entities/FavoriteSalesItem";
+import OwnSalesItem from "../../../salesitem/types/entities/OwnSalesItem";
+import FollowedUserAccount from "../entities/FollowedUserAccount";
+import FollowingUserAccount from "../entities/FollowingUserAccount";
+import _IdAndCaptcha from "../../../../backk/types/id/_IdAndCaptcha";
+import IsStrongPassword from "../../../../backk/decorators/typeproperty/IsStrongPassword";
+import IsUndefined from "../../../../backk/decorators/typeproperty/IsUndefined";
+import _Id from "../../../../backk/types/id/_Id";
+import MaxLengthAndMatches from "../../../../backk/decorators/typeproperty/MaxLengthAndMatches";
+import IsStringOrObjectId from "../../../../backk/decorators/typeproperty/IsStringOrObjectId"; // eslint-disable-next-line @typescript-eslint/class-name-casing
+import { ArrayMaxSize, ArrayMinSize, IsPhoneNumber, MaxLength, IsEmail, IsString, IsAscii } from "class-validator"
+import { ManyToMany } from "../../../../backk/decorators/typeproperty/ManyToMany"
+import { Lengths } from "../../../../backk/constants/constants"
+import { ShouldBeTrueFor } from "../../../../backk/decorators/typeproperty/ShouldBeTrueFor"
+import { OneToMany } from "../../../../backk/decorators/typeproperty/OneToMany"
+import { Private } from "../../../../backk/decorators/typeproperty/Private"
+import { Unique } from "../../../../backk/decorators/typeproperty/Unique"
+import { Transient } from "../../../../backk/decorators/typeproperty/Transient"
+import { BackkEntity } from "../../../../backk/types/entities/BackkEntity"
+
 
 export default class ChangeUserPasswordArg {
   @IsStringOrObjectId({
@@ -34,4 +58,5 @@ export default class ChangeUserPasswordArg {
   @IsString()
   @IsStrongPassword()
   public repeatNewPassword!: string;
+
 }
