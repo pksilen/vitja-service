2ca6a9aa8dca874be0c7a74083bac4de0e98bf32
Petri Silen <petri.silen@silensoft.com>
2021-03-25 16:22:41 +0200

    Changes

--- src/services/order/types/args/PayOrderArg.ts
+++ src/services/order/types/args/PayOrderArg.ts
@@ -2,29 +2,15 @@
 // DO NOT MODIFY THIS FILE! Updates should be made to the respective .type file only
 // This file can be generated from the respective .type file by running npm script 'generateTypes'
 
-import Entity from "../../../../backk/decorators/entity/Entity";
-import OrderItem from "../entities/OrderItem";
-import _IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestampAndUserAccountId from "../../../../backk/types/id/_IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestampAndUserAccountId";
-import MinMax from "../../../../backk/decorators/typeproperty/MinMax";
-import _IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestamp from "../../../../backk/types/id/_IdAndVersionAndCreatedAtTimestampAndLastModifiedTimestamp";
-import IsStringOrObjectId from "../../../../backk/decorators/typeproperty/IsStringOrObjectId";
-import MaxLengthAndMatches from "../../../../backk/decorators/typeproperty/MaxLengthAndMatches"; // eslint-disable-next-line @typescript-eslint/class-name-casing
-import _IdAndVersionAndCreatedAtTimestamp from "../../../../backk/types/id/_IdAndVersionAndCreatedAtTimestamp";
-import IsUndefined from "../../../../backk/decorators/typeproperty/IsUndefined";
-import _IdAndVersion from "../../../../backk/types/id/_IdAndVersion";
-import IsUndefined from "../../../../backk/decorators/typeproperty/IsUndefined"; // eslint-disable-next-line @typescript-eslint/class-name-casing
-import _Id from "../../../../backk/types/id/_Id";
-import IsIntegerStringOrAny from "../../../../backk/decorators/typeproperty/IsIntegerStringOrAny"; // eslint-disable-next-line @typescript-eslint/class-name-casing
-import MaxLengthAndMatches from "../../../../backk/decorators/typeproperty/MaxLengthAndMatches";
-import IsStringOrObjectId from "../../../../backk/decorators/typeproperty/IsStringOrObjectId"; // eslint-disable-next-line @typescript-eslint/class-name-casing
-import { ArrayMaxSize, ArrayMinSize, IsAlphanumeric, MaxLength, IsDate, IsString } from "class-validator"
-import { Lengths, Values } from "../../../../backk/constants/constants"
-import { OneToMany } from "../../../../backk/decorators/typeproperty/OneToMany"
-import { PaymentGateway } from "../enum/PaymentGateway"
-import { IsExternalId } from "../../../../backk/decorators/typeproperty/IsExternalId"
-import { IsFloat } from "../../../../backk/decorators/typeproperty/IsFloat"
-import { BackkEntity } from "../../../../backk/types/entities/BackkEntity"
-
+import { IsAlphanumeric, MaxLength } from 'class-validator';
+import { Lengths, Values } from '../../../../backk/constants/constants';
+import { IsExternalId } from '../../../../backk/decorators/typeproperty/IsExternalId';
+import { IsFloat } from '../../../../backk/decorators/typeproperty/IsFloat';
+import IsStringOrObjectId from '../../../../backk/decorators/typeproperty/IsStringOrObjectId';
+import IsUndefined from '../../../../backk/decorators/typeproperty/IsUndefined';
+import MaxLengthAndMatches from '../../../../backk/decorators/typeproperty/MaxLengthAndMatches'; // eslint-disable-next-line @typescript-eslint/class-name-casing
+import MinMax from '../../../../backk/decorators/typeproperty/MinMax';
+import { PaymentGateway } from '../enum/PaymentGateway';
 
 export default class PayOrderArg {
   @IsUndefined({
@@ -50,5 +36,4 @@ export default class PayOrderArg {
   @IsFloat(2)
   @MinMax(0, Values._1B)
   public paymentAmount!: number | null;
-
 }
